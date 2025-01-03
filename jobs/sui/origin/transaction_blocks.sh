e3c78dd2b36726978d39d6a373139f94671c3e51
lakechain <lakechain.nguyen@gmail.com>
2025-01-03 11:44:39 +0000

    update rpc for sui

--- jobs/sui/origin/transaction_blocks.sh
+++ jobs/sui/origin/transaction_blocks.sh
@@ -3,5 +3,5 @@ $CHAINSLAKE_HOME_DIR/spark/script/chainslake-run.sh --class chainslake.sui.Main 
     --master local[4] \
     --driver-memory 4g \
     --conf "spark.app_properties.app_name=sui_origin.transaction_blocks" \
-    --conf "spark.app_properties.rpc_list=https://fullnode.mainnet.sui.io" \
+    --conf "spark.app_properties.rpc_list=$SUI_RPCS" \
     --conf "spark.app_properties.config_file=sui/application.properties"
